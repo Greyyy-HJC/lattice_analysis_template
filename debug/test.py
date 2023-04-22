48ed8f7849ec29314463714654f93dc0a687f071
Greyyy-HJC <hejinchen17@gmail.com>
2023-04-21 20:50:15 -0400

    resampling_funcions

--- debug/test.py
+++ debug/test.py
@@ -1,13 +1,29 @@
 import numpy as np
 import h5py as h5
 
-from liblattice.preprocess.resampling import bootstrap
+from liblattice.general.general_plot_funcs import *
+from liblattice.preprocess.resampling import *
 
-# generate a 3 dimensional x list to test bootstrap function
-x = np.random.rand(10, 6, 3)
+# generate a 2 dimensional x list to test bootstrap function
+x = np.random.rand(100, 3, 3)
 
-res = bootstrap(x, 5, axis=1)
-print(np.shape(res))
-print(res[0])
+bs = bootstrap(x, 50, axis=0)
+print(np.shape(bs))
+
+gv_ls_1 = gv.dataset.avg_data(bs, bstrap=True)
+
+gv_ls_2 = bs_ls_avg(bs)
+
+distribution = gv_ls_to_samples_corr(gv_ls_2, 100)
+
+gv_ls_3 = gv.dataset.avg_data(distribution, bstrap=True)
+
+
+# make a errorbar list plot with three lists
+x_ls = [np.arange(10), np.arange(10), np.arange(10)]
+y_ls = [gv.mean(gv_ls_1), gv.mean(gv_ls_2), gv.mean(gv_ls_3)]
+yerr_ls = [gv.sdev(gv_ls_1), gv.sdev(gv_ls_2), gv.sdev(gv_ls_3)]
+
+errorbar_ls_plot(x_ls, y_ls, yerr_ls, label_ls=['1','2','3'], title="test", ylim=None, save=True)
 
 
--- liblattice/general/general_plot_funcs.py
+++ liblattice/general/general_plot_funcs.py
@@ -55,7 +55,7 @@ def fill_between_plot(x, y, yerr, title, ylim=None, save=True):
         x,
         [y[i] + yerr[i] for i in range(len(y))],
         [y[i] - yerr[i] for i in range(len(y))],
-        alpha=0.4
+        alpha=0.4,
     )
     ax.tick_params(direction="in", top="on", right="on", **ls_p)
     ax.grid(linestyle=":")
@@ -67,5 +67,61 @@ def fill_between_plot(x, y, yerr, title, ylim=None, save=True):
         plt.savefig("output/plots/" + title + "_fill.pdf", transparent=True)
 
 
+def errorbar_ls_plot(x_ls, y_ls, yerr_ls, label_ls, title, ylim=None, save=True):
+    """Make a general errorbar plot with multiple lines, default save to `output/plots/`.
+
+    Args:
+        x_ls (list): list of list of float x values
+        y_ls (list): list of list of float y values
+        yerr_ls (list): list of list of float yerr values
+        label_ls (list): list of str labels
+        title (str): title of the plot, and also the name of the plot file
+        ylim (tuple, optional): set the ylim of the plot. Defaults to None.
+        save (bool, optional): whether save it. Defaults to True.
+    """
+
+    fig = plt.figure(figsize=fig_size)
+    ax = plt.axes(plt_axes)
+    for x_ls, y_ls, yerr_ls, label_ls in zip(x_ls, y_ls, yerr_ls, label_ls):
+        ax.errorbar(x_ls, y_ls, yerr_ls, label=label_ls, **errorb)
+    ax.tick_params(direction="in", top="on", right="on", **ls_p)
+    ax.grid(linestyle=":")
+    ax.set_ylim(ylim)
+    plt.title(title, **fs_p)
+    plt.legend()
+
+    if save == True:
+        plt.savefig("output/plots/" + title + "_err_ls.pdf", transparent=True)
 
 
+def fill_between_ls_plot(x_ls, y_ls, yerr_ls, label_ls, title, ylim=None, save=True):
+    """Make a general fill_between plot with multiple lines, default save to `output/plots/`.
+
+    Args:
+        x_ls (list): list of list of float x values
+        y_ls (list): list of list of float y values
+        yerr_ls (list): list of list of float yerr values
+        label_ls (list): list of str labels
+        title (str): title of the plot, and also the name of the plot file
+        ylim (tuple, optional): set the ylim of the plot. Defaults to None.
+        save (bool, optional): whether save it. Defaults to True.
+    """
+
+    fig = plt.figure(figsize=fig_size)
+    ax = plt.axes(plt_axes)
+    for x_ls, y_ls, yerr_ls, label_ls in zip(x_ls, y_ls, yerr_ls, label_ls):
+        ax.fill_between(
+            x_ls,
+            [y_ls[i] + yerr_ls[i] for i in range(len(y_ls))],
+            [y_ls[i] - yerr_ls[i] for i in range(len(y_ls))],
+            alpha=0.4,
+            label=label_ls,
+        )
+    ax.tick_params(direction="in", top="on", right="on", **ls_p)
+    ax.grid(linestyle=":")
+    ax.set_ylim(ylim)
+    plt.title(title, **fs_p)
+    plt.legend()
+
+    if save == True:
+        plt.savefig("output/plots/" + title + "_fill_ls.pdf", transparent=True)
