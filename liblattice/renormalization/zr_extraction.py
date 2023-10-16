82c7a5ae2af01339efa9d6417d3a00aef032b473
Greyyy-HJC <hejinchen17@gmail.com>
2023-10-16 13:10:39 -0400

    renormalization

---
+++ liblattice/renormalization/zr_extraction.py
@@ -0,0 +1,12 @@
+""" 
+Use zero momentum matrix elements to extract renormalization factors zR.
+You can find an example usage at the end of this file.
+"""
+
+import numpy as np
+import lsqfit as lsf
+
+
+if __name__ == "__main__":
+    #todo example usage
+    pass
