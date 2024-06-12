6b09cc99b5c918f4c4802f0362e0559b3d2b88c9
Greyyy-HJC <hejinchen17@gmail.com>
2024-06-12 15:20:36 -0400

    plot_sum_fit_on_data

--- liblattice/general/general_plot_funcs.py
+++ liblattice/general/general_plot_funcs.py
@@ -187,7 +187,7 @@ def errorbar_fill_between_ls_plot(
         plt.savefig("../output/plots/" + title + "_err_fill_ls.pdf", transparent=True)
 
 
-def plot_fit_on_data_log(get_ratio_data, ra_fit_res, ra_re_fcn, ra_im_fcn, px, py, pz, b, z, err_tsep_ls, fill_tsep_ls, Ls, err_tau_cut=1, fill_tau_cut=1):
+def plot_ratio_fit_on_data_log(get_ratio_data, ra_fit_res, ra_re_fcn, ra_im_fcn, px, py, pz, b, z, err_tsep_ls, fill_tsep_ls, Ls, err_tau_cut=1, fill_tau_cut=1):
     """
     Plot the ratio fit on data.
 
@@ -268,3 +268,55 @@ def plot_fit_on_data_log(get_ratio_data, ra_fit_res, ra_re_fcn, ra_im_fcn, px, p
 
     # Plot imaginary part
     plot_part('imag', ra_im_avg, ra_im_fcn, 'pdf_im')
+
+
+def plot_sum_fit_on_data_log(get_sum_data, sum_fit_res, sum_re_fcn, sum_im_fcn, px, py, pz, b, z, err_tsep_ls, fill_tsep_ls, err_tau_cut=2, fill_tau_cut=2):
+    """
+    Plot the sum fit on data.
+
+    Args:
+        px (float): Momentum in the x-direction.
+        py (float): Momentum in the y-direction.
+        pz (float): Momentum in the z-direction.
+        b (float): Impact parameter.
+        z (float): Light-cone momentum fraction.
+        ss_fit_res (FitResult): Fit result for the 2pt SS fit.
+        err_tsep_ls (list): List of time separations for error bars.
+        fill_tsep_ls (list): List of time separations for filled regions.
+        err_tau_cut (int, optional): Cut for the range of tau values used for error bars. Defaults to 1.
+        fill_tau_cut (int, optional): Cut for the range of tau values used for filled regions. Defaults to 1.
+
+    Returns:
+        None
+    """
+    from liblattice.preprocess.resampling import bs_ls_avg
+
+    sum_re, sum_im = get_sum_data(px, py, pz, b, z, err_tsep_ls, jk_bs="bs", tau_cut=err_tau_cut)
+
+    # Reshape and average the data only once.
+    sum_re_avg = bs_ls_avg(sum_re)
+    sum_im_avg = bs_ls_avg(sum_im)
+
+    def plot_part(part, sum_avg, sum_fcn):
+        fit_t = np.linspace(fill_tsep_ls[0]-0.5, fill_tsep_ls[-1]+0.5, 100)
+        fit_sum = sum_fcn(fit_t, fill_tau_cut, sum_fit_res.p)
+
+        x_ls = [np.array(err_tsep_ls), fit_t]
+        y_ls = [gv.mean(sum_avg), gv.mean(fit_sum)]
+        yerr_ls = [gv.sdev(sum_avg), gv.sdev(fit_sum)]
+        label_ls = ['data', 'fit']
+        plot_style_ls = ['errorbar', 'fill_between']
+
+        fig = plt.figure(figsize=fig_size)
+        ax = plt.axes(plt_axes)
+        errorbar_fill_between_ls_plot(
+            x_ls, y_ls, yerr_ls, label_ls, plot_style_ls,
+            title=f'Sum_fit_on_data_P{px}_{part}_b{b}_z{z}', save=False, head=ax
+        )
+        plt.savefig(f"../log/gsfit/Sum_fit_on_data_P{px}_{part}_b{b}_z{z}.pdf", transparent=True)
+
+    # Plot real part
+    plot_part('real', sum_re_avg, sum_re_fcn)
+
+    # Plot imaginary part
+    plot_part('imag', sum_im_avg, sum_im_fcn)
